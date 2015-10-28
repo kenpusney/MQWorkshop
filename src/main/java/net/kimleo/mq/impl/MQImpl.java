8c334ca242f026950401b7ce0d6a4b3b603257c8
KimmyLeo <kenpusney@gmail.com>
2015-10-28 18:08:55 +0800

    fix compile error & warnings

---
+++ src/main/java/net/kimleo/mq/impl/MQImpl.java
@@ -0,0 +1,30 @@
+package net.kimleo.mq.impl;
+
+import net.kimleo.mq.*;
+
+public class MQImpl implements MQ {
+    @Override
+    public Producer createProducer() {
+        return null;
+    }
+
+    @Override
+    public Consumer createConsumer() {
+        return null;
+    }
+
+    @Override
+    public Queue createQueue(String id) {
+        return null;
+    }
+
+    @Override
+    public Topic createTopic(String id) {
+        return null;
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
