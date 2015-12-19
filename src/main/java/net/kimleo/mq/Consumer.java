41b1639fcdba3761398f560251f6d7dd0de4432c
KimmyLeo <kenpusney@gmail.com>
2015-12-19 12:13:38 +0800

    organize import

--- src/main/java/net/kimleo/mq/Consumer.java
+++ src/main/java/net/kimleo/mq/Consumer.java
@@ -1,7 +1,5 @@
 package net.kimleo.mq;
 
-import sun.security.krb5.internal.crypto.Des;
-
 public interface Consumer extends Observer<Destination> {
     <T> void acceptOnce(Destination destination, Handler<Message<T>> handler);
     <T> void listen(Destination destination, Handler<Message<T>> handler);
